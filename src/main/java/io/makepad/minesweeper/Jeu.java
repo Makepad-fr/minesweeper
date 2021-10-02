b39812842eb4deab12c86d80ad7a776cdc91d4cc
idilsaglam <idilsaglam1@hotmail.com>
2021-10-02 19:38:25 +0200

    feat(core): Jeu clas updated

--- src/main/java/io/makepad/minesweeper/Jeu.java
+++ src/main/java/io/makepad/minesweeper/Jeu.java
@@ -1,4 +1,12 @@
 package io.makepad.minesweeper;
 
 public class Jeu {
+    Joueur joueur;
+    Plateau plateau;
+
+    public Jeu(Joueur j, Plateau p){
+        this.joueur = j;
+        this.plateau = p;
+    }
+
 }
