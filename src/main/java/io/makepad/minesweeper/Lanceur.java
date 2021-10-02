83b379a42346ac9985818b79a376cc7fe91f8697
idilsaglam <idilsaglam1@hotmail.com>
2021-10-02 19:39:36 +0200

    feat(core): Lanceur class updated

--- src/main/java/io/makepad/minesweeper/Lanceur.java
+++ src/main/java/io/makepad/minesweeper/Lanceur.java
@@ -4,5 +4,7 @@ public class Lanceur {
     public static void main(String[] args) {
         Plateau p = new Plateau(20, 20, 10);
         p.afficheTout();
+
+        Joueur j = new Joueur();
     }
 }
